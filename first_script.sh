e1d91a73bdc44fe58c638111c14e6e79492d3faf
HugoItaloMC <magalhaeshugocorreia@gmail.com>
2023-09-21 14:03:32 -0300

    INITIAL COMMIT

---
+++ first_script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+############################################
+# Autor: github.com/hugoitalomc            #
+# Data Inicio: 09/20/2023                  #
+#                                          #
+# Desc. Conceitos Básicos de shell script  #
+#                                          #
+############################################
+
+echo "-- INICIANDO NO SHELL SCRIPT --"
+
+# Capturando dados do usuário
+
+read -p "INSIRA UM DADO: " INPUT
+
+echo ""
+echo "$INPUT FOI O DADO INFORMADO"
